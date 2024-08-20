73c664e428525ba4e7d99c4a3cb67dfb9662a16a
teodor-valchev <teo.thenx@gmail.com>
2024-08-20 10:58:45 +0300

    - created component for a single item from the catalog, - displaying the cocktails on the home page

--- client/src/components/catalog/Catalog.jsx
+++ client/src/components/catalog/Catalog.jsx
@@ -1,66 +1,23 @@
 import { useEffect, useState } from "react";
 
 import * as cocktailsService from "../../coctails_API/cocktailsService.js";
-
-import { IconHeart } from "@tabler/icons-react";
-import {
-    Card,
-    Image,
-    Text,
-    Group,
-    Badge,
-    Button,
-    ActionIcon,
-} from "@mantine/core";
-import classes from "./Catalog.module.css";
+import Item from "../item/Item.jsx";
 
 const Catalog = () => {
     const [cocktails, setCocktails] = useState([]);
 
+    console.log(cocktails);
+    
+
     useEffect(() => {
-        cocktailsService.getAll()
-            .then((res) => setCocktails(res.drinks));
+        cocktailsService.getAll().then((res) => setCocktails(res.drinks));
     }, []);
 
-    console.log(cocktails);
-
-    return (
-        <Card withBorder radius="md" p="md" className={classes.card}>
-            <Card.Section>
-                {/* <Image src={''} alt={title} height={180} /> */}
-            </Card.Section>
-
-            <Card.Section className={classes.section} mt="md">
-                <Group justify="apart">
-                    <Text fz="lg" fw={500}>
-                        {/* {title} */}
-                    </Text>
-                    <Badge size="sm" variant="light">
-                        {/* {country} */}
-                    </Badge>
-                </Group>
-                <Text fz="sm" mt="xs">
-                    {/* {description} */}
-                </Text>
-            </Card.Section>
-
-            <Card.Section className={classes.section}>
-                <Text mt="md" className={classes.label} c="dimmed">
-                    Perfect for you, if you enjoy
-                </Text>
-                <Group gap={7} mt={5}></Group>
-            </Card.Section>
-
-            <Group mt="xs">
-                <Button radius="md" style={{ flex: 1 }}>
-                    Show details
-                </Button>
-                <ActionIcon variant="default" radius="md" size={36}>
-                    <IconHeart className={classes.like} stroke={1.5} />
-                </ActionIcon>
-            </Group>
-        </Card>
-    );
+    return cocktails.map((cocktail) => (
+        <Item
+            key={cocktail.idDrink}
+            {...cocktail} />
+    ));
 };
 
 export default Catalog;
