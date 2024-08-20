1074aebfbefc1751deb7e1e0dc3040d9a200b106
teodor-valchev <teo.thenx@gmail.com>
2024-08-20 13:17:48 +0300

    Added a not found page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,14 @@ import { MantineProvider } from "@mantine/core";
 import Catalog from "./components/catalog/Catalog";
 
 import "@mantine/core/styles.css";
+import NotFound from "./components/404/NotFound";
 
 function App() {
     return (
         <MantineProvider>
             <Routes>
                 <Route path="/" element={<Catalog />} />
+                <Route path="*" element={<NotFound />} />
             </Routes>
         </MantineProvider>
     );
--- client/src/coctails_API/cocktailsService.js
+++ client/src/coctails_API/cocktailsService.js
@@ -1,10 +1,29 @@
 const URL =
     "https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary_Drink";
 
+const Single_Cocktaile_URL =
+    "https://www.thecocktaildb.com/api/json/v1/1/lookup.php";
+
 export const getAll = async () => {
-    const data = await fetch(URL);
+    try {
+        const data = await fetch(URL);
 
-    const result = await data.json();
+        const result = await data.json();
 
-    return result;
+        return result;
+    } catch (error) {
+        alert("Something went wrong...");
+    }
+};
+
+export const getOne = async (id) => {
+    try {
+        const data = await fetch(`${Single_Cocktaile_URL}i=${id}`);
+
+        const result = await data.json();
+
+        return result;
+    } catch (error) {
+        alert("Something went wrong...");
+    }
 };
