976a9a733645c43b073a2dcda0a7fb82cbd48c09
teodor-valchev <teo.thenx@gmail.com>
2024-08-20 13:29:53 +0300

    Created details page view

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,12 +5,14 @@ import Catalog from "./components/catalog/Catalog";
 
 import "@mantine/core/styles.css";
 import NotFound from "./components/404/NotFound";
+import Details from "./components/details/Details";
 
 function App() {
     return (
         <MantineProvider>
             <Routes>
                 <Route path="/" element={<Catalog />} />
+                <Route path="/details/:id" element={<Details />} />
                 <Route path="*" element={<NotFound />} />
             </Routes>
         </MantineProvider>
